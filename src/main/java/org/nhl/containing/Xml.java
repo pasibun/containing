b88bbd95c481de11870fc2e0961f6ad590b42c97
MorenoB <moreno_bralts@hotmail.com>
2014-11-28 11:12:38 +0100

    fixed xml

--- src/main/java/org/nhl/containing/Xml.java
+++ src/main/java/org/nhl/containing/Xml.java
@@ -1,31 +1,39 @@
 package org.nhl.containing;
 
+import org.w3c.dom.CharacterData;
 import org.w3c.dom.*;
 import org.xml.sax.InputSource;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.StringReader;
 
 enum Command {
+
     Create, Move, Dispose, LastMessage
 }
 
 /**
- *
- *
+ * Parses provided XML files and returns Containers.
  */
 public class Xml {
 
-    public static String okObject;
-    public static int okId;
+    public static int maxValueContainers;
+    public static String containerIso;
+    public static String containerOwner;
+    public static String transportType;
+    public static String objectName;
+    public static String destinationName;
+    public static String speed;
     public static Command command;
+    public static int xLoc;
+    public static int yLoc;
+    public static int zLoc;
 
     /**
      * Tries to decode the incoming XML message and splits it within attributes
      * of this class.
-     *
-     * @param xmlMessage The xml message you're willing to decode
+     *     
+* @param xmlMessage The xml message you're willing to decode
      */
     public static void decodeXMLMessage(String xmlMessage) {
         try {
@@ -35,19 +43,95 @@ public class Xml {
             InputSource is = new InputSource();
             is.setCharacterStream(new StringReader(xmlMessage));
             Document doc = db.parse(is);
-            NodeList nodes = doc.getElementsByTagName("OK");
+            NodeList nodes = doc.getElementsByTagName("LastMessage");
             if (nodes.getLength() > 0) {
                 for (int i = 0; i < nodes.getLength(); i++) {
                     Element element = (Element) nodes.item(i);
-                    NodeList numberOfContainers = element.getElementsByTagName("OBJECT");
+                    NodeList numberOfContainers = element.getElementsByTagName("numberOfContainers");
                     Element line = (Element) numberOfContainers.item(0);
-                    okObject = getCharacterDataFromElement(line);
-                    System.out.println("OBJECT: " + okObject);
-                    numberOfContainers = element.getElementsByTagName("OBJECTID");
-                    line = (Element) numberOfContainers.item(0);
-                    okId = Integer.parseInt(getCharacterDataFromElement(line));
-                    System.out.println("OBJECTID: " + okId);
+                    maxValueContainers = Integer.parseInt(getCharacterDataFromElement(line));
+                    System.out.println("numberOfContainers: " + maxValueContainers);
                 }
+                containerIso = null;
+                containerOwner = null;
+                transportType = null;
+                objectName = null;
+                destinationName = null;
+                speed = null;
+                command = Command.LastMessage;
+            }
+            nodes = doc.getElementsByTagName("Create");
+            if (nodes.getLength() > 0) {
+                for (int i = 0; i < nodes.getLength(); i++) {
+                    Element element = (Element) nodes.item(i);
+                    NodeList nodeList = element.getElementsByTagName("iso");
+                    Element line = (Element) nodeList.item(0);
+                    containerIso = getCharacterDataFromElement(line);
+                    System.out.println("ISO: " + containerIso);
+                    nodeList = element.getElementsByTagName("owner");
+                    line = (Element) nodeList.item(0);
+                    containerOwner = getCharacterDataFromElement(line);
+                    System.out.println("Owner: " + containerOwner);
+                    nodeList = element.getElementsByTagName("arrivalTransportType");
+                    line = (Element) nodeList.item(0);
+                    transportType = getCharacterDataFromElement(line);
+                    System.out.println("arrivalTransportType: " + transportType);
+                    nodeList = element.getElementsByTagName("arrivalTransportType");
+                    line = (Element) nodeList.item(0);
+                    xLoc = Integer.parseInt(getCharacterDataFromElement(line));
+                    System.out.println("xLoc: " + xLoc);
+                    nodeList = element.getElementsByTagName("arrivalTransportType");
+                    line = (Element) nodeList.item(0);
+                    yLoc = Integer.parseInt(getCharacterDataFromElement(line));
+                    System.out.println("yLoc: " + yLoc);
+                    nodeList = element.getElementsByTagName("arrivalTransportType");
+                    line = (Element) nodeList.item(0);
+                    zLoc = Integer.parseInt(getCharacterDataFromElement(line));
+                    System.out.println("zLoc: " + zLoc);
+                }
+                objectName = null;
+                destinationName = null;
+                speed = null;
+                command = Command.Create;
+            }
+            nodes = doc.getElementsByTagName("Move");
+            if (nodes.getLength() > 0) {
+                for (int i = 0; i < nodes.getLength(); i++) {
+                    Element element = (Element) nodes.item(i);
+                    NodeList nodeList = element.getElementsByTagName("objectName");
+                    Element line = (Element) nodeList.item(0);
+                    objectName = getCharacterDataFromElement(line);
+                    System.out.println("objectName: " + objectName);
+                    nodeList = element.getElementsByTagName("destinationName");
+                    line = (Element) nodeList.item(0);
+                    destinationName = getCharacterDataFromElement(line);
+                    System.out.println("destinationName: " + destinationName);
+                    nodeList = element.getElementsByTagName("speed");
+                    line = (Element) nodeList.item(0);
+                    speed = getCharacterDataFromElement(line);
+                    System.out.println("speed: " + speed);
+                }
+                containerIso = null;
+                containerOwner = null;
+                transportType = null;
+                command = Command.Move;
+            }
+            nodes = doc.getElementsByTagName("Dispose");
+            if (nodes.getLength() > 0) {
+                for (int i = 0; i < nodes.getLength(); i++) {
+                    Element element = (Element) nodes.item(i);
+                    NodeList nodeList = element.getElementsByTagName("objectName");
+                    Element line = (Element) nodeList.item(0);
+                    objectName = getCharacterDataFromElement(line);
+                    System.out.println("objectName: " + objectName);
+                }
+                containerIso = null;
+                containerOwner = null;
+                transportType = null;
+                objectName = null;
+                destinationName = null;
+                speed = null;
+                command = Command.Dispose;
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -64,5 +148,49 @@ public class Xml {
             return cd.getData();
         }
         return "?";
+    }
+
+    public static Command getCommand() {
+        return command;
+    }
+
+    public static String getContainerIso() {
+        return containerIso;
+    }
+
+    public static String getContainerOwner() {
+        return containerOwner;
+    }
+
+    public static String getDestinationName() {
+        return destinationName;
+    }
+
+    public static int getMaxValueContainers() {
+        return maxValueContainers;
+    }
+
+    public static String getObjectName() {
+        return objectName;
+    }
+
+    public static String getSpeed() {
+        return speed;
+    }
+
+    public static String getTransportType() {
+        return transportType;
+    }
+
+    public static int getxLoc() {
+        return xLoc;
+    }
+
+    public static int getyLoc() {
+        return yLoc;
+    }
+
+    public static int getzLoc() {
+        return zLoc;
     }
 }
